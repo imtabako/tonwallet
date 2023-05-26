881ff644797f4e41b6524647aedb3b2a03ad354f
imtabako <imtabako@gmail.com>
2023-05-26 22:52:56 +0300

    activities

--- src/main/java/com/example/firsttest/CongratsActivity.kt
+++ src/main/java/com/example/firsttest/CongratsActivity.kt
@@ -2,6 +2,7 @@ package com.example.firsttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class CongratsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- src/main/java/com/example/firsttest/StartActivity.kt
+++ src/main/java/com/example/firsttest/StartActivity.kt
@@ -3,17 +3,26 @@ package com.example.firsttest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toolbar
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
 
         findViewById<Button>(R.id.start_btn_create)
             .setOnClickListener {
-                val intent = Intent(this, LoginActivity::class.java)
+                val intent = Intent(this, CongratsActivity::class.java)
                 startActivity(intent)
             }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return true
     }
 }
